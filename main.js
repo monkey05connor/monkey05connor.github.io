eba49f6c356a5caf79bdcfe84e3f72323c88dfd9
monkey05connor <42944992+monkey05connor@users.noreply.github.com>
2018-09-04 17:56:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+	$("#purchase").on("click", () => {
+		window.location = "monkey05connor.github.io/purchase.html";
+	});
+	$("#admin").on("click", () => {
+		window.location = "monkey05connor.github.io/administrate.html";
+	});
+});
